0b148a02a8fa6cfb799cb047ad9e3decf708934a
Adilkhan Altynbek <adilkhanaltynbek@MBPA.local>
2025-08-04 14:50:41 +0500

    Коммит-2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
-#!/bin/sh
-# Ждём пока база данных будет доступна (необязательно, но полезно)
-until php artisan migrate:status > /dev/null 2>&1; do
-  echo "Waiting for database..."
-  sleep 3
-done
-
 # Запускаем миграции
 php artisan migrate --force
 
