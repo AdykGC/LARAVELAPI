fab887a5085b993876c6eeec686b40ae311dc19e
Altynbek Adilkhan <adilkhanaltynbek@MBPA.local>
2025-08-13 15:18:25 +0500

    Fork | Commit | 3

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,12 +4,18 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
 
 class RoleSeeder extends Seeder {
     public function run(): void {
-        Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => 'Website Admin']);
-        Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => "Teacher"]);
-        Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => "Student"]);
+        $R1 = Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => 'Website Admin']);
+        $R2 = Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => "Teacher"]);
+        $R3 = Role::firstOrCreate(['guard_name' => 'sanctum', 'name' => "Student"]);
+
+        Permission::create(["name" => "View | ALL"]);
+        Permission::create(["name" => "EDIT | ALL"]);
+        Permission::create(["name" => "A"]);
+        Permission::create(["name" => "B"]);
 
         $USR1 = User::firstOrCreate([
             "full_name"=> "Junior Admin RIKS",
@@ -17,7 +23,7 @@ class RoleSeeder extends Seeder {
             "email" => "Mandarin@Carbonated.drink",
             "password" => Hash::make("Qaz123edc"),
         ]);
-        $USR1->assignRole('Website Admin');
+        $USR1->assignRole($R1);
 
         $USR2 = User::firstOrCreate([
             "full_name"=> "Lester",
@@ -25,7 +31,7 @@ class RoleSeeder extends Seeder {
             "email" => "Farin@coffee.drink",
             "password" => Hash::make("Qaz123edc"),
         ]);
-        $USR2->assignRole('Teacher');
+        $USR2->assignRole($R2);
 
         $USR3 = User::firstOrCreate([
             "full_name"=> "Adidas",
@@ -33,6 +39,11 @@ class RoleSeeder extends Seeder {
             "email" => "Mand@Adid.com",
             "password" => Hash::make("Qaz123edc"),
         ]);
-        $USR3->assignRole('Student');
+        $USR3->assignRole($R3);
+        
+        
+        $R1->syncPermissions(['View | ALL', 'EDIT | ALL']);
+        $R2->syncPermissions(['A']);
+        $R3->syncPermissions(['B']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -29,57 +29,38 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-// Защищённые маршруты
-Route::middleware('auth:sanctum')->group(function () {
-    // для всех пользователей в системе
-    Route::get('/courses',                    getCoursesController::class);
-});
-#
-#
-#
-#
 Route::post('/register',                  RegisterUsersController::class);
 Route::post('/login',                     LoginUsersController::class);
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout',                             LogoutUsersController::class);
-    # SHOW
-    Route::get('/showRoles',                           ShowRolesController::class);
-    Route::get('/showPermissions',                     ShowPermissionsController::class);
-    Route::get('/showPermissionsByRole',               ShowPermissionsByRoleController::class);
-    Route::get('/showStudents',                        ShowStudentsListController::class);
-    Route::get('/showTeachers',                        ShowTeachersListController::class);
-    Route::get('/showWebAdmins',                       ShowWebsiteAdminsListController::class);
-    # INFORMATION
     Route::get('/showMe',                              InformationCurrentUser::class);
-    # UPDATE
-    Route::put('/putPermissionsToRole',                PutPermissionsToRole::class);
-    Route::put('/putAnotherRoleToUser',                PutAnotherRoleToUser::class);
-    Route::post('/createCourseController',             CreateCourseController::class);
-    Route::post('/createUserController',               CreateUserController::class);
-    Route::get('/courses/my',                          ShowEnrolledCoursesController::class);
-    Route::post('/courses/enroll',                     EnrollToCourseController::class);
-    Route::post('/courses/unenroll',                   UnenrollToCourseController::class);
-
-
+    Route::get('/courses',                    getCoursesController::class);
 });
-#Route::middleware(['auth:sanctum', 'student'])->group(function () {
-#    Route::patch('/student/profile',          updateProfileController::class);
-#    Route::post('/student/ETC/{ID}',          enrollCourseController::class);
-#    Route::post('/student/UETC/{ID}',         unenrollCourseController::class);
-#    Route::get('/student/my/course',          getMyCourseController::class);
-#    Route::post('/student/my/course/{ID}',    getMyCourseByIDController::class);
-#});
-#
-#
-#
-##
-#Route::post('/teacher/login',                  TEAloginController::class);
-#Route::middleware(['auth:sanctum', 'teacher'])->group(function () {
 
-#    Route::post('/teacher/logout',            TEAlogoutController::class);
-#    Route::post('/teacher/ETC/{ID}',          TEAenrollController::class);
-#    Route::post('/teacher/UETC/{ID}',         TEAunenrollController::class);
-#    Route::get('/teacher/my/course',          getTeaMyCourseController::class);
-#    Route::post('/teacher/my/course/{ID}',    getTeaMyCourseByIDController::class);
-#});
+Route::group(['middleware' => function ($request, $next) {
+    if (! ($request->user()->can('A') || $request->user()->can('B'))) {
+        abort(403);
+    }
+    return $next($request);
+}], function () {
+    Route::get('/showMe', InformationCurrentUser::class);
+    Route::get('/courses/my', ShowEnrolledCoursesController::class);
+    Route::post('/courses/enroll', EnrollToCourseController::class);
+    Route::post('/courses/unenroll', UnenrollToCourseController::class);
+});
+
+Route::middleware('can:EDIT | ALL')->group(function () {
+    Route::put('/putPermissionsToRole',                PutPermissionsToRole::class);
+    Route::post('/create/courses',                     CreateCourseController::class);
+    Route::post('/create/users',                       CreateUserController::class);
+    Route::get('/showWebAdmins',                       ShowWebsiteAdminsListController::class);
+    Route::get('/showTeachers',                        ShowTeachersListController::class);
+    Route::get('/showPermissions',                     ShowPermissionsController::class);
+    Route::put('/putAnotherRoleToUser',                PutAnotherRoleToUser::class);
+});
+
+Route::middleware('can:View | ALL')->group(function () {
+    Route::get('/showPermissionsByRole',               ShowPermissionsByRoleController::class);
+    Route::get('/showRoles',                           ShowRolesController::class);
+    Route::get('/showStudents',                        ShowStudentsListController::class);
+});
