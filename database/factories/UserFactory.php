b4a3de5837790a54e4b055c97a0afb610325c6b0
Altynbek Adilkhan <adilkhanaltynbek@MBPA.local>
2025-08-13 16:13:50 +0500

    Revert "Revert "TEST""
    This reverts commit 14ff64087279ae549327e198fe80415d6d4e8879.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,40 +3,21 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
-class UserFactory extends Factory
-{
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
+class UserFactory extends Factory {
+    public function definition(): array {
         return [
-            'name' => fake()->name(),
+            'full_name' => fake()->name(),
+            'university_email' => fake()->unique()->safeEmail(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'phone' => fake()->unique()->phoneNumber(),
+            'password' => bcrypt('Qaz123edc'),
         ];
     }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
+    public function unverified(): static {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
