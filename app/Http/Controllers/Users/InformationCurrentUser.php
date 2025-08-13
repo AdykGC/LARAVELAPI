bee5ccfd81f06e362ee0daf77aae75b2f63892f5
Altynbek Adilkhan <adilkhanaltynbek@MBPA.local>
2025-08-13 16:17:35 +0500

    Revert "TEST"
    This reverts commit f685698e2629d7c28f9db781a4fe64baa4598eb6.

--- app/Http/Controllers/Users/InformationCurrentUser.php
+++ app/Http/Controllers/Users/InformationCurrentUser.php
@@ -5,7 +5,6 @@ use App\Models\User;
 
 class InformationCurrentUser extends BaseController{
     public function __invoke() {
-        dd('here');
         /** @var User $user */
         $user = auth()->user();
         return $this->service->getCurrentUserInformation($user);
